ab0873cc3cc8d9a6d3dbfdac2f55279d15d88ca8
ngetich720 <bkipchirchir720@gmail.com>
2022-12-07 10:29:24 +0000

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
-        continue
-    else:
-        print("{}".format(chr(i)), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end=""))
