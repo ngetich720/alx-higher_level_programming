337ae1054e379a4f9f6bde79d9064d2c9b4b202b
ngetich720 <bkipchirchir720@gmail.com>
2023-07-25 05:54:36 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- listing all privilages for user
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
